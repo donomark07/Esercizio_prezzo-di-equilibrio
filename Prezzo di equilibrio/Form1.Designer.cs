260421fc02ce9068fddfebede8bfd98bf9587922
DONOFRIO.21215 <DONOFRIO.21215@LABI1-13PC03>
2025-11-26 12:28:34 +0100

    Inserimento grafico

--- Prezzo di equilibrio/Form1.Designer.cs
+++ Prezzo di equilibrio/Form1.Designer.cs
@@ -28,14 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea1 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend1 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series1 = new System.Windows.Forms.DataVisualization.Charting.Series();
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.tb_q = new System.Windows.Forms.TextBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(61, 232);
+            this.button1.Location = new System.Drawing.Point(27, 68);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(146, 55);
             this.button1.TabIndex = 0;
@@ -46,7 +51,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(58, 199);
+            this.label1.Location = new System.Drawing.Point(24, 35);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(59, 13);
             this.label1.TabIndex = 1;
@@ -54,21 +59,39 @@
             // 
             // tb_q
             // 
-            this.tb_q.Location = new System.Drawing.Point(126, 198);
+            this.tb_q.Location = new System.Drawing.Point(92, 34);
             this.tb_q.Name = "tb_q";
             this.tb_q.Size = new System.Drawing.Size(80, 20);
             this.tb_q.TabIndex = 2;
+            // 
+            // chart1
+            // 
+            chartArea1.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea1);
+            legend1.Name = "Legend1";
+            this.chart1.Legends.Add(legend1);
+            this.chart1.Location = new System.Drawing.Point(252, 54);
+            this.chart1.Name = "chart1";
+            series1.ChartArea = "ChartArea1";
+            series1.Legend = "Legend1";
+            series1.Name = "Series1";
+            this.chart1.Series.Add(series1);
+            this.chart1.Size = new System.Drawing.Size(327, 248);
+            this.chart1.TabIndex = 3;
+            this.chart1.Text = "chart1";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.chart1);
             this.Controls.Add(this.tb_q);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -79,6 +102,7 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox tb_q;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
     }
 }
 
--- Prezzo di equilibrio/Form1.cs
+++ Prezzo di equilibrio/Form1.cs
@@ -17,6 +17,14 @@ namespace Prezzo_di_equilibrio
             InitializeComponent();
         }
 
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+        }
+
+
         private void button1_Click(object sender, EventArgs e)
         {
             if (string.IsNullOrEmpty(tb_q.Text))
@@ -38,6 +46,27 @@ namespace Prezzo_di_equilibrio
             int a = 78;
 
             MessageBox.Show("Domanda: " + d + "\nOfferta: " + o);
+
+
+
+            chart1.Series.Clear();
+            chart1.Titles.Add("Grafico domanda-offerta");
+
+            chart1.Series.Add("Domanda");
+            chart1.Series["Domanda"].Color = Color.Green;
+            chart1.Series["Domanda"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+
+            chart1.Series.Add("Offerta");
+            chart1.Series["Offerta"].Color = Color.Blue;
+            chart1.Series["Offerta"].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Point;
+
+            chart1.Series["Domanda"].Points.AddXY(1, 40);
+            chart1.Series["Domanda"].Points.AddXY(2, 60);
+            chart1.Series["Domanda"].Points.AddXY(3, 80);
+            chart1.Series["Domanda"].Points.AddXY(4, 100);
+            chart1.Series["Offerta"].Points.AddXY(5, 110);
+            chart1.Series["Offerta"].Points.AddXY(6, 80);
+            chart1.Series["Offerta"].Points.AddXY(7, 70);
         }
     }
 }
